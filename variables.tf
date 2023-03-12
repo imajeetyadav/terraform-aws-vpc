a09ffce245d0a7a45517833d60ebf5c3c84080b0
Ajeet Yadav <ajeetyadav164@gmail.com>
2023-03-13 02:04:21 +0530

    sort terraform variables

--- variables.tf
+++ variables.tf
@@ -1,16 +1,21 @@
-variable "name" {
-  description = "the name of your stack"
-  type        = string
-}
-
-variable "region" {
-  description = "the AWS region in which resources are created, you must set the availability_zones variable as well if you define this value to something other than the default"
-  type        = string
+variable "availability_zones" {
+  description = "List of availability zones"
+  type        = list(any)
 }
 
 variable "cidr" {
   description = "The CIDR block for the VPC."
   type        = string
+}
+
+variable "name" {
+  description = "the name of your stack"
+  type        = string
+}
+
+variable "private_subnets" {
+  description = "List of private subnets"
+  type        = list(any)
 }
 
 variable "public_subnets" {
@@ -19,12 +24,7 @@ variable "public_subnets" {
 
 }
 
-variable "private_subnets" {
-  description = "List of private subnets"
-  type        = list(any)
-}
-
-variable "availability_zones" {
-  description = "List of availability zones"
-  type        = list(any)
+variable "region" {
+  description = "the AWS region in which resources are created, you must set the availability_zones variable as well if you define this value to something other than the default"
+  type        = string
 }
