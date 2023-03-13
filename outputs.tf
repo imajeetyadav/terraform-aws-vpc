f40442c8d5a7a1c5f214271e4dd2e6b293a8a842
Ajeet Yadav <ajeetyadav164@gmail.com>
2023-03-13 15:01:27 +0530

    🎨 Output variable renamed

--- outputs.tf
+++ outputs.tf
@@ -2,12 +2,12 @@ output "vpc_id" {
   description = "The ID of the VPC"
   value       = try(aws_vpc.vpc.id, "")
 }
-output "public_subnets" {
+output "public_subnet_ids" {
   description = "List of IDs of public subnets"
   value       = aws_subnet.public[*].id
 }
 
-output "private_subnets" {
+output "private_subnet_ids" {
   description = "List of IDs of private subnets"
   value       = aws_subnet.private[*].id
 }
